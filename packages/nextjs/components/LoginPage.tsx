be7d816719004e7d0d12cec80f4d9c6961256d30
Esther <esther@acceleratorapp.co>
2024-05-27 18:22:20 +0200

    styling login page

---
+++ packages/nextjs/components/LoginPage.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function LoginPage() {
+  return (
+    <div className="relative flex">
+      <h1 className="px-20 pt-[10%] text-7xl text-white">The decentralized cloud is here</h1>
+    </div>
+  );
+}
