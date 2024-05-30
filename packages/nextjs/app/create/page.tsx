929e18fe9b9f6ca84878de3b85a0625b8fce7af8
Esther <esther@acceleratorapp.co>
2024-05-31 00:09:51 +0200

    change background value

--- packages/nextjs/app/create/page.tsx
+++ packages/nextjs/app/create/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { useAccount } from "wagmi";
 import { MagnifyingGlassIcon } from "@heroicons/react/20/solid";
 import LoginPage from "~~/components/LoginPage";
--- packages/nextjs/components/LoginPage.tsx
+++ packages/nextjs/components/LoginPage.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function LoginPage() {
   return (
-    <div className="relative h-[100vh] -mt-14 flex items-center bg-connect bg-cover bg-no-repeat">
+    <div className="relative h-[100vh] -mt-14 flex items-center bg-wallet bg-cover bg-no-repeat">
       <div className="relative h-[100vh] w-full flex items-center  bg-black bg-opacity-50 ">
         <h1 className="px-20 text-7xl font-[500] text-white w-[55rem]">The decentralized blog is here</h1>
       </div>
