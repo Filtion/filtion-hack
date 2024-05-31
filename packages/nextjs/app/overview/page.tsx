3e05bfa7cb13f128592e5db65a95b794f38b3c32
Esther <esther@acceleratorapp.co>
2024-05-31 22:56:56 +0200

    add extra menu in header for navigation

---
+++ packages/nextjs/app/overview/page.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const testData = [
+  {
+    title: "Test blog about everything",
+    url: "skitpond/frogmind",
+    date: new Date(),
+  },
+  {
+    title: "How to write a blog?",
+    url: "skitpond/howto",
+    date: new Date(),
+  },
+  {
+    title: "What is a DAO?",
+    url: "skitpond/vamos",
+    date: new Date(),
+  },
+  {
+    title: "Where do we stand on market disruption",
+    url: "disruption",
+    date: new Date(),
+  },
+  {
+    title: "What is Defi?",
+    url: "skitpond/defimain",
+    date: new Date(),
+  },
+];
+
+export default function Overview() {
+  return <div></div>;
+}
