ff0ee93649578c12e98612fa272d89f68a0c210f
SaharshSS <67810603+SaharshSS@users.noreply.github.com>
2024-06-01 08:04:33 -0700

    Update BlogSubdomains

--- packages/ens-contracts/contracts/wrapper/BlogSubdomain.sol
+++ packages/ens-contracts/contracts/wrapper/BlogSubdomain.sol
@@ -5,13 +5,15 @@ import "packages/ens-contracts/contracts/INameWrapper.sol";
 import "@openzeppelin/contracts/token/ERC1155/IERC1155Receiver.sol";
 import "@openzeppelin/contracts/utils/introspection/ERC165.sol";
 import { useAccount } from "wagmi";
+import { useState } from "react";
+
 
 contract Subdomains {
     setSubnodeOwner(
         useAccount, // The namehash of the parent node, e.g. "myname.eth"
-        "y", // The label of the subname to create
+        useState, // The label of the subname to create
         useAccount, // The address you want to be the owner of the new subname
-        int z, // The fuse bits OR'd togethe,r that you want to burn
+        68877, // The fuse bits OR'd togethe,r that you want to burn
         1717427392 // The expiry for the subname (Monday, June 3, 2024 in this case)
     )
 }
